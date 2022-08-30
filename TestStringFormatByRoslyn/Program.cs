fdf4b8fbe0f6e455947a7a4882ffe2f54ca3fca7
Kaizhi Xuan <xuankaizhi@aacoptics.com>
2022-08-30 14:25:44 +0800

    test dynamicexpresso

--- TestStringFormatByRoslyn/Program.cs
+++ TestStringFormatByRoslyn/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using DynamicExpresso;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Net.Utilities.Object.Collection;
+using Net.Utilities.Object.String;
+using Net.Utilities.Struct.Byte;
+using Net.Utilities.Struct.DateTime;
+using Net.Utilities.Struct.Enum;
+using Net.Utilities.Struct.IntPtr;
+using Net.Utilities.Struct.Numeric;
 using TestStringFormatByRoslyn;
 
 
@@ -14,4 +22,30 @@ var test = await CSharpScript.EvaluateAsync<string>(str, globals: new ParamModel
     }
 });
 Console.WriteLine(test);
+var interpreter = new Interpreter()
+    .Reference(typeof(Array))
+    .Reference(typeof(Enumerable))
+    .Reference(typeof(ByteCastHelper))
+    .Reference(typeof(CollectionCastHelp))
+    .Reference(typeof(DateTimeCastHelper))
+    .Reference(typeof(NumberCastHelper))
+    .Reference(typeof(EnumCastHelper))
+    .Reference(typeof(IntPtrCastHelper))
+    .Reference(typeof(StringCastHelper));
+object o = 1;
+var result = interpreter.Eval("row.ToInt32OrDefault()+1", new Parameter("row", o));
+Console.WriteLine(result);
+result = interpreter.Eval("row+1", new Parameter("row", o));
+Console.WriteLine(result);
+o = "202208301112";
+result = interpreter.Eval("DateTimeCastHelper.String2DateTime(row,\"yyyyMMddHHmm\")", new Parameter("row", o));
+Console.WriteLine(result);
+
+result = interpreter.Eval("row+1", new Parameter("row", o));
+Console.WriteLine(result);
+
+o = 4; //0100
+result = interpreter.Eval("(row >> 2 & 1) == 1", new Parameter("row", o));
+Console.WriteLine(result);
+
 Console.ReadKey();
